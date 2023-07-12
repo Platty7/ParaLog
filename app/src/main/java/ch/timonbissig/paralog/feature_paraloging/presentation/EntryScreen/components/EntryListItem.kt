92fc00befcb08caa4d35486beec63014b2a1e47c
Timon Bissig <timon.bissig@pse-solutions.ch>
2023-07-12 15:48:58 +0200

    add card with template

--- app/src/main/java/ch/timonbissig/paralog/feature_paraloging/presentation/EntryScreen/components/EntryListItem.kt
+++ app/src/main/java/ch/timonbissig/paralog/feature_paraloging/presentation/EntryScreen/components/EntryListItem.kt
@@ -2,17 +2,23 @@ package ch.timonbissig.paralog.feature_paraloging.presentation.EntryScreen.compo
 
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,25 +31,35 @@ fun EntryListItem(icon: ImageVector, date:Int, title:String, level:String) {
         .fillMaxWidth()
         .wrapContentHeight(),
     shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondary
+        )
 
 
     ){
-        Column(Modifier.padding(8.dp)) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.headlineLarge,
-                color = MaterialTheme.colorScheme.onPrimary,
-            )
-            Text(
-                text = level,
-                style = MaterialTheme.typography.bodyMedium,
-            )
+        Row() {
+            androidx.compose.material3.Icon(imageVector = icon, contentDescription = "Icons", modifier = Modifier
+                .padding(start = 6.dp)
+                .size(50.dp)
+                .align(CenterVertically))
+            Column(Modifier.padding(8.dp)) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.onTertiary,
+                )
+                Text(
+                    text = level,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
         }
+
     }
 }
 
 @Preview
 @Composable
 fun EntryListItemPreview() {
-    EntryListItem(Icons.Default.Home, 234, "Hallo", "Level 1")
+    EntryListItem(Icons.Default.Home, 234, "25.066.2023", "Level 1")
 }
